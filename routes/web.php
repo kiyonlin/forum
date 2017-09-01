b7f783788a9f254dac8ab97dc4db466904a0a68f
kiyon <kiyonlin@163.com>
2017-09-01 09:37:48 +0800

    the episode 11: users can filter threads by channel

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,9 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/threads', 'ThreadsController@index');
+Route::get('/threads/create', 'ThreadsController@create');
 Route::get('/threads/{channel}/{thread}', 'ThreadsController@show');
-Route::resource('/threads', 'ThreadsController');
+Route::post('/threads', 'ThreadsController@store');
+Route::get('/threads/{channel}', 'ThreadsController@index');
 Route::post('/threads/{channel}/{thread}/replies', 'RepliesController@store');
