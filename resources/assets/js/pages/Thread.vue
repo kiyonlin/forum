3057526ddb549828e11b923944621cdc68b6382e
kiyon <kiyonlin@163.com>
2017-11-27 14:08:09 +0800

    86 put lock button

--- resources/assets/js/pages/Thread.vue
+++ resources/assets/js/pages/Thread.vue
@@ -1,16 +1,17 @@
 <script>
-    import Replies from '../components/Replies.vue';
-    import SubscribeButton from '../components/SubscribeButton.vue';
+import Replies from "../components/Replies.vue";
+import SubscribeButton from "../components/SubscribeButton.vue";
 
-    export default {
-        props: ['initialRepliesCount'],
+export default {
+  props: ["dataRepliesCount", "dataLocked"],
 
-        components: {Replies, SubscribeButton},
+  components: { Replies, SubscribeButton },
 
-        data() {
-            return {
-                repliesCount: this.initialRepliesCount
-            }
-        }
-    }
+  data() {
+    return {
+      repliesCount: this.dataRepliesCount,
+      locked: this.dataLocked
+    };
+  }
+};
 </script>
