8a9a6af7b88ad4e18dd15cc2da767a1ca57359a5
kiyon <kiyonlin@163.com>
2017-11-29 16:32:51 +0800

    94 search scout review

--- app/Thread.php
+++ app/Thread.php
@@ -4,11 +4,12 @@ namespace App;
 
 use App\Events\ThreadReceivedNewReply;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 
 class Thread extends Model
 {
 
-    use RecordsActivity;
+    use RecordsActivity, Searchable;
 
     protected $guarded = [];
 
